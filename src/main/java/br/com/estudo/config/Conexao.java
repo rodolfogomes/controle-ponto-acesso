e55bbf1925447b8dc279667de5f2bdae10f36fc5
rodolfogomes <rodolfoaguiargomes@gmail.com>
2021-11-25 20:15:45 -0300

    add conexao com mysql

---
+++ src/main/java/br/com/estudo/config/Conexao.java
@@ -0,0 +1,21 @@
+package br.com.estudo.config;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	
+	private static  Connection conn = null;
+	private static final String USUARIO = "root";
+    private static final String SENHA = "root";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/controle-ponto-acesso";
+	
+	public static Connection getConnection() throws SQLException {
+		
+			return DriverManager.getConnection(URL, USUARIO, SENHA);
+	}
+
+}
