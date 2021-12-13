4681adc19d995a9cf1282411593781416c0d32b7
Rodolfo Gomes <rodolfoaguiargomes@gmail.com>
2021-12-13 16:16:04 -0300

    ajust commit

---
+++ controle-ponto-acesso-spring/src/main/java/com/example/controlepontoacessospring/ControlePontoAcessoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlepontoacessospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePontoAcessoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlePontoAcessoSpringApplication.class, args);
+    }
+
+}
